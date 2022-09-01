593dfdf916aba9ef045bd5879a1f8282ed615592
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-09-01 09:08:49 -0500

    Update LSP

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -31,13 +31,13 @@ return require('packer').startup(function(use)
         use({
             "hrsh7th/nvim-cmp",
             requires = {
-                  { "hrsh7th/cmp-buffer", after = "nvim-cmp" },
-                  { "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" },
-                  { "hrsh7th/cmp-path", after = "nvim-cmp" },
-                  { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" },
-                  { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
+                  { "hrsh7th/cmp-buffer"},
+                  { "hrsh7th/cmp-nvim-lsp"},
+                  { "hrsh7th/cmp-path"},
+                  { "hrsh7th/cmp-nvim-lua"},
+                  { "saadparwaiz1/cmp_luasnip"},
             },
-		    config = function() require('plugins.nvim-cmp') end
+		    config = function() require('lsp') end
 	})
 
 	    -- luasnip
@@ -82,7 +82,6 @@ return require('packer').startup(function(use)
         -- Lualine
         use {
           'nvim-lualine/lualine.nvim',
-          requires = { 'kyazdani42/nvim-web-devicons', opt = true },
           config = function() require('plugins.lualine') end
         }
         
@@ -106,10 +105,4 @@ return require('packer').startup(function(use)
             config = function() require('nvim-tree').setup() end
         }
         
-        -- Debugger
-        use { 
-            'rcarriga/nvim-dap-ui', 
-            requires = {'mfussenegger/nvim-dap'} 
-        }
-          
 end)
