e5ef021fc13a6b1d57fde36819ad8ee361fb9fcd
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-09-01 16:22:34 -0500

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,6 +41,7 @@ return require('packer').startup(function(use)
                   { "hrsh7th/cmp-nvim-lsp"},
                   { "hrsh7th/cmp-path"},
                   { "hrsh7th/cmp-nvim-lua"},
+                  { "hrsh7th/cmp-cmdline"},
                   { "saadparwaiz1/cmp_luasnip"},
             },
 		    config = function() require('lsp') end
