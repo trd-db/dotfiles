12161147985c82793eaac919aa3304a48de34613
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-09-02 10:59:59 -0500

    Update

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -20,19 +20,24 @@ vim.keymap.set('n', '<leader>fh', function() require("telescope.builtin").help_t
 -- [[ Hop ]]
 --------------------------------------------------
 local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-keymap("n", '<leader><leader>w', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-keymap("n", '<leader><leader>b', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-keymap("n", '<leader><leader>e', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-keymap("n", '<leader><leader>f', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
-keymap("n", '<leader><leader>t', "<cmd>lua require'hop'.hint_char2()<cr>", opts)
-keymap("n", '<leader><leader>/', "<cmd>lua require'hop'.hint_patterns()<cr>", opts)
-keymap("n", '<leader><leader>j', "<cmd>lua require'hop'.hint_lines()<cr>", opts)
-keymap("n", '<leader><leader>k', "<cmd>lua require'hop'.hint_lines()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>w', "<cmd>lua require'hop'.hint_words()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>b', "<cmd>lua require'hop'.hint_words()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>e', "<cmd>lua require'hop'.hint_words()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>f', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>t', "<cmd>lua require'hop'.hint_char2()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>/', "<cmd>lua require'hop'.hint_patterns()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>j', "<cmd>lua require'hop'.hint_lines()<cr>", opts)
+vim.keymap.set("n", '<leader><leader>k', "<cmd>lua require'hop'.hint_lines()<cr>", opts)
 
-keymap("v", '<leader><leader>w', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-keymap("v", '<leader><leader>b', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-keymap("v", '<leader><leader>o', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
-keymap("v", '<leader><leader>t', "<cmd>lua require'hop'.hint_char2()<cr>", opts)
-keymap("v", '<leader><leader>/', "<cmd>lua require'hop'.hint_patterns()<cr>", opts)
-keymap("v", '<leader><leader>l', "<cmd>lua require'hop'.hint_lines()<cr>", opts)
+vim.keymap.set("v", '<leader><leader>w', "<cmd>lua require'hop'.hint_words()<cr>", opts)
+vim.keymap.set("v", '<leader><leader>b', "<cmd>lua require'hop'.hint_words()<cr>", opts)
+vim.keymap.set("v", '<leader><leader>o', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+vim.keymap.set("v", '<leader><leader>t', "<cmd>lua require'hop'.hint_char2()<cr>", opts)
+vim.keymap.set("v", '<leader><leader>/', "<cmd>lua require'hop'.hint_patterns()<cr>", opts)
+vim.keymap.set("v", '<leader><leader>l', "<cmd>lua require'hop'.hint_lines()<cr>", opts)
+
+
+-- [[ Misc ]]
+--------------------------------------------------
+-- Leader q closes buffer
+vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>", opts)
